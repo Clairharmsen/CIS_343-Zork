fb46abd399c9bf9005fc7b63c152cbb5434b6be7
Ezekiel Turnbough <turnbou1@msu.edu>
2025-02-18 12:25:37 -0500

    Fixed the exception handling, item should be done besides adding comments and teammate names.

--- Item.cpp
+++ Item.cpp
@@ -9,61 +9,17 @@
 #include <string>
 
 
-Item::Item(std::string n, std::string d, int c, float w)
+Item::Item(const std::string& n, const std::string& d, int c, float w)
 {
-    if (n.size() == 0)
-    {
-        std::cout << "Name must not be empty\n";
-        return;
-    }
-    else
-    {
-        name = n;
-    }
-
-    if (d.size() == 0)
-    {
-        std::cout << "Description must not be empty\n";
-        return;
-    }
-    else
-    {
-        description = d;
-    }
-
-    if ((c < 0) or (c > 1000))
-    {
-        std::cout << "Calories must be between 0 and 1000\n";
-        return;
-    }
-    else
-    {
-        calories = c;
-    }
-
-
-    if ((w < 0) or (w > 500))
-    {
-        std::cout << "Weight must be between 0 and 500\n";
-        return;
-    }
-    else
-    {
-        weight = w;
-    }
+    SetName(n);
+    SetDescription(d);
+    SetCalories(c);
+    SetWeight(w);
 }
 
-std::ostream & operator <<(std::ostream & output, Item & item)
+std::ostream & operator <<(std::ostream & output, const Item & item)
 {
-    if ((item.GetName().size() == 0) or (item.GetDescription().size() == 0) or ((0 > item.GetCalories())
-    or (item.GetCalories() > 1000)) or ((item.GetWeight() < 0) or (item.GetWeight() > 500)))
-    {
-        output << "There is an error with one of the values.\n";
-    }
-    else
-    {
-        output << item.GetName() << " (" << item.GetCalories() << " calories) - " << item.GetWeight() << " lb - " << item.GetDescription() << "\n";
-    }
+    output << item.GetName() << " (" << item.GetCalories() << " calories) - " << item.GetWeight() << " lb - " << item.GetDescription() << "\n";
     return output;
 }
 
--- Item.hpp
+++ Item.hpp
@@ -19,9 +19,9 @@ class Item
         int calories = 0;
         float weight = 0.f;
     public:
-        Item(std::string n, std::string d, int c, float w);
+        Item(const std::string& n, const std::string& d, int c, float w);
 
-        friend std::ostream & operator <<(std::ostream & output, Item & item1);
+        friend std::ostream & operator <<(std::ostream & output, const Item & item1);
 
         std::string GetName() const {return name;}
         std::string GetDescription() const {return description;}
@@ -29,10 +29,31 @@ class Item
         float GetWeight() const {return weight;}
 
 
-        void SetName(std::string n) {name = n;}
-        void SetDescription(std::string d) {description = d;}
-        void SetCalories(int cal) {calories = cal;}
-        void SetWeight(float w) {weight = w;}
+        void SetName(const std::string& n)
+        {
+            if (n.size() <= 0) {
+                throw std::invalid_argument("Item name cannot be empty");
+            }
+            name = n;
+        }
+        void SetDescription(const std::string& d) {
+            if (d.size()<= 0) {
+                throw std::invalid_argument("Item description cannot be empty");
+            }
+            description = d;
+        }
+        void SetCalories(int cal) {
+            if ((0 > cal) or (cal > 1000)) {
+                throw std::invalid_argument("Item calories out of range. Must be between 0 and 1000");
+            }
+            calories = cal;
+        }
+        void SetWeight(float w) {
+            if ((0 > w) or (w > 500)){
+                throw std::invalid_argument("Item weight out of range. Must be between 0 and 500");
+            }
+            weight = w;
+        }
 };
 
 
